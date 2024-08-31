f8f0c1b09807ed99e80fdae693082032b13e363a
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-08-31 18:36:27 +0100

    feat: added a flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-""" 
-0. Basic Flask app
-"""
+'''Task 0: Basic Flask app
+'''
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('./0-index.html')
 
-# flask --app hello run --debug
+@app.route('/')
+def index():
+    '''default route'''
+    return render_template("0-index.html",)
+
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
